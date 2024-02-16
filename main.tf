5ccbf00d7e4de037c76cbeaaa55609321e0ad06d
JesusHoracio-CastilloMoralesGit <jesus.castillomorales07@gmail.com>
2024-02-16 12:33:03 -0600

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>7.1"
+    }
+  }
+  backend "s3" {
+    bucket = "tato-terraform-state-bucket"
+    key = "terraform/terraform.tfstate"
+    dynamodb_table = "tato-terraform-state-lock"
+    region = "us-east-1"
+  }
+}
